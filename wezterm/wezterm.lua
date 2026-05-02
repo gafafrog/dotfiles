53a09970af0b600f744b023ec5ebd626dd3eb51f
gafafrog <code@gafafrog.com>
2026-05-02 07:26:28 -0700

    Add blur

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,7 @@ config.scrollback_lines = 10000
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
 config.window_background_opacity = 0.8
+config.macos_window_background_blur = 4
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 config.macos_forward_to_ime_modifier_mask = 'CTRL|SHIFT'
